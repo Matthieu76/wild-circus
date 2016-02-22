328736037029f72d5d98fd5904d638f7f0d32bf7
Alexis Phuong <alexis.hvp@gmail.com>
2016-02-22 23:33:45 +0100

    Ajout d'un script pour animer le background

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.title').click(function(){
+		$("#background01").fadeOut(2000, function(){
+			$("#background01").attr('id','background02');
+			$(".title").hide();
+			$("#background02").fadeIn('slow');
+			$("#content").fadeIn(2000);
+		});
+	});
+});
