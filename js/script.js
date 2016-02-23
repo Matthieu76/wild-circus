50cd6815b1a69011310418bd5a8803105c2abcff
Alexis Phuong <alexis.hvp@gmail.com>
2016-02-23 15:40:03 +0100

    Amélioration du script.js

--- js/script.js
+++ js/script.js
@@ -7,4 +7,16 @@ $(document).ready(function(){
 			$(".content").fadeIn(2000);
 		});
 	});
+
+	$('#shows').click(function(){
+		$("#background01").fadeOut(2000, function(){
+			$("#background01").attr('id','background02');
+			$("li:first").removeClass("active");
+			$("ul li:nth-child(2)").addClass("active");
+			$(".title").hide();
+			$("#background02").fadeIn('slow');
+			$(".content").fadeIn(2000);
+		});
+	});
+
 });
