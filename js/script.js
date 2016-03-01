39a1fc281316662b1a837a006469427dcb586639
Alexis Phuong <alexis.hvp@gmail.com>
2016-03-01 20:49:10 +0100

    Finalisation du projet

--- js/script.js
+++ js/script.js
@@ -2,15 +2,6 @@ $(document).ready(function(){
 	$('.title').click(function(){
 		$("#background01").fadeOut(2000, function(){
 			$("#background01").attr('id','background02');
-			$(".title").hide();
-			$("#background02").fadeIn('slow');
-			$(".content").fadeIn(2000);
-		});
-	});
-
-	$('#shows').click(function(){
-		$("#background01").fadeOut(2000, function(){
-			$("#background01").attr('id','background02');
 			$("li:first").removeClass("active");
 			$("#shows").addClass("active");
 			$(".title").hide();
@@ -19,4 +10,18 @@ $(document).ready(function(){
 		});
 	});
 
+	
+$(document).ready(function(){
+	$('#shows').click(function(){
+			$("#background01").fadeOut(2000, function(){
+			$("#background01").attr('id','background02');
+			$("li:first").removeClass("active");
+			$("#shows").addClass("active");
+			$(".title").hide();
+			$("#background02").fadeIn('slow');
+			$(".content").fadeIn(2000);
+			});
+		});
+	});
+		
 });
